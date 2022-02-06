67cf71027017b59676478aa869740c0880f4f824
sanskarsri <sanskar0703@gmail.com>
2022-02-07 03:08:50 +0530

    build

---
+++ client/build/precache-manifest.66c6c929c541f1293c54987c71607ece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359db1c5cbe2f0a4e3c8435a5988fb58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98064f8c13f95b74a9e4",
+    "url": "/static/css/2.0f95a132.chunk.css"
+  },
+  {
+    "revision": "1fc1c47202baf8e22ef2",
+    "url": "/static/css/main.78b7a195.chunk.css"
+  },
+  {
+    "revision": "98064f8c13f95b74a9e4",
+    "url": "/static/js/2.26457ecb.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/static/js/2.26457ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc1c47202baf8e22ef2",
+    "url": "/static/js/main.df496cce.chunk.js"
+  },
+  {
+    "revision": "77b8dad9641a813b46f8",
+    "url": "/static/js/runtime-main.ea6d0dd5.js"
+  }
+]);
